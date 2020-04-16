31a5f88492c9809d11450fb8d509d99621735e1a
Lukas Geiger <lukas.geiger94@gmail.com>
2020-04-16 18:16:32 +0100

    Add model summary renderer

---
+++ model_summary.py
@@ -0,0 +1,16 @@
+import larq_zoo as lqz
+import larq as lq
+import io
+from functools import reduce
+
+
+def html_format(source, language, css_class, options, md):
+    model_fn = reduce(getattr, [lqz, *source.split(".")])
+    stream = io.StringIO()
+    lq.models.summary(model_fn(weights=None), print_fn=lambda s: print(s, file=stream))
+    return f"""
+    <details class="abstract"
+      ><summary>Model Summary</summary>
+      <pre class="model-summary"><code>{stream.getvalue()}</code></pre>
+    </details>
+    """
