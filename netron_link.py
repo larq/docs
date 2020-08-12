0fb6934947ed2d1ba4895d328d4288b4ca776fcc
Lukas Geiger <lukas.geiger94@gmail.com>
2020-08-12 14:52:25 +0200

    Fix netron link

--- netron_link.py
+++ netron_link.py
@@ -3,5 +3,5 @@ cors_proxy = "https://cors-anywhere.herokuapp.com"
 release_url = "https://github.com/larq/zoo/releases/download"
 
 
-def html_format(source, language, css_class, options, md):
+def html_format(source, language, css_class, options, md, **kwargs):
     return f'<div class="admonition abstract"><a class="netron-link" href="{netron_link}/?url={cors_proxy}/{release_url}/{source}"><p>Interactive architecture diagram</p></a></div>'
